e5cf33adb01a349cd66fb922854467b14284f320
Josh <josh@gmail.com>
2022-09-29 22:58:07 -0500

    Net Pay Update

--- NetPay.py
+++ NetPay.py
@@ -9,22 +9,6 @@ deduction3 = p.PayrollDeduction('food court', '8/17/2022', 15.25, 21547)
 deduction4 = p.PayrollDeduction('vending machine', '8/22/2022', 3.00, 58475)
 deduction5 = p.PayrollDeduction('vending machine', '8/5/2022', 2.75, 58475)
 
-#Table 1 
-print('Name         ', 'ID Number  ', ' Department  ', '           Job Title  ', 'Monthly Salary  ')
-print('-----------------------------------------------------------------------------------------')
-print(employee.getName(),' ', employee.getID(),'      ', employee.getDepartment(), '   ', employee.getJobTitle(),' '+ '$'+ f'{employee.getSalary(): ,.2f}')
-print()
-
-#Table 2
-print('Description  ', '     Date  ', '     Charge', '   EmployeeID')
-print('--------------------------------------------------------------')
-print(deduction1.getDescription(), '       ',deduction1.getDate(),' ' ,deduction1.getCharge(), '    ', deduction1.getEmpID())
-print(deduction2.getDescription(), '',deduction2.getDate(),' ' ,deduction2.getCharge(), '    ', deduction2.getEmpID())
-print(deduction3.getDescription(), '       ',deduction3.getDate(),' ' ,deduction3.getCharge(), '   ', deduction3.getEmpID())
-print(deduction4.getDescription(), '  ',deduction4.getDate(),' ' ,deduction4.getCharge(), '     ', deduction4.getEmpID())
-print(deduction5.getDescription(), '  ',deduction5.getDate(),'  ' ,deduction5.getCharge(), '    ', deduction5.getEmpID())
-print()
-
 #Report
 print('*** Employee Pay ***')
 print('Name:', employee.getName())
